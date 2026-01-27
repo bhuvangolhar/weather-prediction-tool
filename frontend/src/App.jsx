d97d65145b47dc9a608254081476a6176aa493a6
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-01-28 00:01:34 +0530

    created a simple webpage for our Weather Prediction System

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Weather Prediction System 🌦️</h1>
+      <p>Check current weather and future forecasts (coming soon).</p>
+
+      <div className="weather-box">
+        Weather data will be displayed here
+      </div>
+
+      <button>Check Weather</button>
+    </div>
+  );
+}
+
+export default App;
